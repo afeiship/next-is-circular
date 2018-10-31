8d607ec3c707ec986c3d965d85af792fbfeb9aae
afeiship <1290657123@qq.com>
2018-11-01 07:30:57 +0800

    issue: done test case

--- test/test.js
+++ test/test.js
@@ -2,18 +2,13 @@ var assert = require('assert');
 var nx = require('next-js-core2');
 require('../src/next-is-circular');
 
-describe('next/isCircular', function () {
+describe('next/isCircular', function() {
+  it('nx.isCircular', function() {
+    var obj1 = { name: 'fei' };
+    var obj2 = { email: '1290657123@qq.com' };
+    obj2.self = obj2;
 
-  it('nx.isCircular', function () {
-    var obj1 = {name: 'fei'};
-    var obj2 = {email: '1290657123@qq.com'};
-
-    var result = {};
-
-    nx.isCircular(result, obj1, obj2);
-
-    assert.equal(result.name, obj1.name);
-    assert.equal(result.email, obj2.email);
+    nx.isCircular(obj1, false);
+    nx.isCircular(obj2, true);
   });
-
 });
